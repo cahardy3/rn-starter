1d9925b2cae81b8a19cdab1a9f1ed13c04fbd20a
cahardy3 <>
2019-11-15 07:28:57 +0000

    More leasrning

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Luke</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Luke1</Text>
+      <Button
+        onPress={() => navigation.navigate('Components')}
+        title="Go To Components Demo"/>
+      <Button
+        onPress={() => navigation.navigate('List')}
+        title="Go To list Demo"/>
+    </View>
+    );
 };
-
 const styles = StyleSheet.create({
   text: {
     fontSize: 45
